9b4993e2c43fafcb276a5d30575d7130f4752e33
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-07-06 12:52:35 +0200

    Add set loading state

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Quotes() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetchQuotes = async () => {
+      setIsLoading(true);
       const api = await fetch('https://api.api-ninjas.com/v1/quotes?category=success', {
         headers: {
           'X-Api-Key': 'j6lZij02bFBGL73iDHbVSA==bDWhyfZiatgBIyiY',
         },
       });
       const response = await api.json();
-      setData(response);
+      setData(response[0].quote);
+      setIsLoading(false);
     };
     fetchQuotes();
-  }, []);
+  }, [setData, setIsLoading]);
+  if (isLoading) return <p>Loading...</p>;
   return (
     <>
-      {console.log(data)}
+      <p>{data}</p>
 
     </>
   );
